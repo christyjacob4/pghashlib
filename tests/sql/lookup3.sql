d4eeb17ecd6fd0f4577171d0a127e33e3af5b429
Christy Jacob <christyjacob4@gmail.com>
2025-08-03 03:43:34 +0400

    refactor: update lookup3 tests to use assertions for consistency checks across text, bytea, and integer inputs

--- tests/sql/lookup3.sql
+++ tests/sql/lookup3.sql
@@ -1,27 +1,27 @@
 -- Test basic hash functionality with text
-SELECT lookup3('hello world');
+SELECT lookup3('hello world') IS NOT NULL;
 
--- Test hash with different text inputs
-SELECT lookup3('test string');
-SELECT lookup3('another test');
+-- Test hash with different text inputs give different results
+SELECT lookup3('test string') != lookup3('hello world');
+SELECT lookup3('another test') != lookup3('hello world');
 
--- Test text input with custom initval
-SELECT lookup3('hello world', 42);
-SELECT lookup3('hello world', 123);
+-- Test text input with custom initval gives different results
+SELECT lookup3('hello world', 42) != lookup3('hello world');
+SELECT lookup3('hello world', 123) != lookup3('hello world', 42);
 
--- Test bytea input
-SELECT lookup3('hello world'::bytea);
+-- Test bytea input consistency
+SELECT lookup3('hello world'::bytea) = lookup3('hello world');
 
 -- Test bytea input with custom initval
-SELECT lookup3('hello world'::bytea, 42);
+SELECT lookup3('hello world'::bytea, 42) = lookup3('hello world', 42);
 
--- Test integer input
-SELECT lookup3(12345);
-SELECT lookup3(-12345);
+-- Test integer input gives consistent results
+SELECT lookup3(12345) = lookup3(12345);
+SELECT lookup3(-12345) = lookup3(-12345);
 
 -- Test integer input with custom initval
-SELECT lookup3(12345, 42);
-SELECT lookup3(-12345, 123);
+SELECT lookup3(12345, 42) = lookup3(12345, 42);
+SELECT lookup3(-12345, 123) != lookup3(-12345, 42);
 
 -- Test consistency (same input should give same hash)
 SELECT lookup3('consistent test') = lookup3('consistent test');
